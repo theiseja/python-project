5f02d2316e1f09bde232187063e15a9dee9adca0
Jesse Theisen <theiseja@gmail.com>
2020-07-13 21:35:42 -0500

    Project setup, added base functions

---
+++ create_playlist.py
@@ -0,0 +1,30 @@
+"""
+Step 1: Log into Youtube
+Step 2: Grab our liked videos
+Step 3: Create a new playlist
+Step 4: Search for the song
+Step 5: Add song into new Spotify playlist
+"""
+import json
+import requests
+from secrets import spotify_user_id
+
+class CreatePlaylist:
+    def __init__(self):
+        pass
+    # Step 1: Log into Youtube
+    def get_youtube_client(self):
+        pass
+    # Step 2: Grab our liked videos
+    def get_liked_videos(self):
+        pass
+    # Step 3: Create a new playlist
+    def create_playlist(self):
+        pass
+    # Step 4: Search for the song
+    def get_spotify_url(self):
+        pass
+    # Step 5: Add song into new Spotify playlist
+    def add_song_to_playlist(self):
+        pass
+    
